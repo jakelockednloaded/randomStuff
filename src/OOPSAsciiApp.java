d5901e91910bfbf84cf4f5e5903c522cd3bba52b
jakelockednloaded <ak0763@srmist.edu.in>
2026-02-10 11:12:54 +0530

    the second one

---
+++ src/OOPSAsciiApp.java
@@ -0,0 +1,14 @@
+public class OOPSAsciiApp {
+    public static void main(String[] args) {
+        // Each letter is 7 lines tall and 9 characters wide
+        // Using string concatenation (+) to build each line
+
+        System.out.println("  *****    *****  " + " *****    *****  ");
+        System.out.println(" **   **  **   ** " + " **  **  **      ");
+        System.out.println("**     ****     **" + " **  **   ****   ");
+        System.out.println("**     ****     **" + " *****       **  ");
+        System.out.println("**     ****     **" + " **          **  ");
+        System.out.println(" **   **  **   ** " + " **      **  **  ");
+        System.out.println("  *****    *****  " + " **       ****   ");
+    }
+}
